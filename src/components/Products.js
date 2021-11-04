c9b3b9e259f43af7439736d3117874e13fd1458e
nciar1 <32080906+nciar1@users.noreply.github.com>
2021-11-04 19:48:48 -0400

    redownloaded missing packages

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import formatCurrency from '../util';
-import Modal from "react-modal";
+import Modal from 'react-modal';
 
 
 export default class Products extends Component {
