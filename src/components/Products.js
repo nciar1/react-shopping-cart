74f3ab516c5d7be230786bc7062db0de79ace04f
nciar1 <32080906+nciar1@users.noreply.github.com>
2021-11-04 17:09:35 -0400

    added animationn to items

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import formatCurrency from '../util'
+import Fade from 'react-reveal/Fade'
 
 export default class Products extends Component {
     render() {
         return (
             <div>
+                <Fade bottom cascade>
                 <ul className="products">
             {this.props.products.map(product => (
                 <li key={product._id}>
@@ -27,6 +29,7 @@ export default class Products extends Component {
                 </li>
             ))}
                 </ul>
+                </Fade>
             </div>
         )
     }
